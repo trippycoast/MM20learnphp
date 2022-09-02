44b038e027a9ec8da6986c517082535b99b0559c
Daniel Koruts <MM-20-766@tptlive.ee>
2022-09-02 15:43:15 +0300

    gugu

--- add.php
+++ add.php
@@ -1,7 +1,19 @@
 <?php
-var_dump($_POST ["newitem"]);
-$File = fopen('List.txt', 'w');
-Fwrite($File, $_POST['newitem'] . "\n");
-Fclose($File);
+    if(trim($_POST['newitem']) && trim($_POST['body'])) {
+        if(file_exists('posts.json')){
+        $json = file_get_contents('posts.json');
+        $posts = json_decode($json, true);
+    } else {
+        $posts = [];
+    }
+        $posts[] = [
+            'title' => $POST['title'],
+            'body' => $POST['body']
+    ];
+    $json = json_encode($posts);
+    $_file = fopen('posts.json', 'w');
+    fwrite($file, $json);
+    fclose($file);
+}
 header('Location: /index.php');
 die();
--- index.php
+++ index.php
@@ -1,29 +1,43 @@
 <?php
-$content = File_get_contenets('List.txt');
-$Items = explode("\n",$content);
-array_pop($Items);
- ?>
+$json = file_get_contents('posts.json');
+$posts = json_decode($json, true);
+?>
     <!DOCTYPE html>
     <html lang="en">
-    <head>
+        <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     </head>
     <body>
-        <form action="/add.php" method="POST">
-            <input type="text" name="newitem">
-            <input type="submit" value="Add">
-        </form>
-    <ul>
-        <?php foreach ($items as $item): ?>
-
-            <li>
-                <?=$item ?>
-            </li>
-
-        <?php endforeach; ?>
+        <div>
+            <a class="button is-primary" href="/new.php">newwha</a>
+                <ul>
+                    <?php 
+                    
+                        foreach ($items as $item): ?>
+                        <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      Component
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="#" class="card-footer-item">Save</a>
+    <a href="#" class="card-footer-item">Edit</a>
+    <a href="#" class="card-footer-item">Delete</a>
+  </footer>
+</div>
+                    <li>
+                        <?=$item ?>
+                    </li>
+                    <?php endforeach;?>
     </ul> 
     </body>
     </html>
